61ba4fdfed1483ffd84c410bedcb54521db6a42f
Ken Tucker <vb2ae@live.com>
2024-11-15 15:45:56 -0500

    Update project file and add new unit tests
    Updated OpenWeatherMap.Standard.csproj:
    - Removed net9.0 target framework
    - Reformatted PropertyGroup and ItemGroup elements
    - Moved Newtonsoft.Json package reference to a new ItemGroup
    
    Added new test classes:
    - CurrentTests: Unit tests for Current class methods, including URL generation and async data retrieval
    - LangValueExtensionTests: Tests for GetStringValue extension method for enums
    - RestServiceCallerTests: Unit tests for RestServiceCaller class using Moq to mock HTTP responses

---
+++ OpenWeatherMap.Standard.Core.Test/Extensions/LangValueExtensionsTests.cs
@@ -0,0 +1,60 @@
+﻿namespace OpenWeatherMap.Standard.Core.Test.Extensions
+{
+    using global::OpenWeatherMap.Standard.Attributes;
+    using global::OpenWeatherMap.Standard.Extensions;
+    using Xunit;
+
+
+    public class LangValueExtensionTests
+    {
+        private enum TestEnum
+        {
+            [LangValue("en")]
+            English,
+            [LangValue("fr")]
+            French,
+            NoLangValue
+        }
+
+        [Fact]
+        public void GetStringValue_WithLangValueAttribute_ReturnsLangValue()
+        {
+            // Arrange
+            var value = TestEnum.English;
+
+            // Act
+            var result = value.GetStringValue();
+
+            // Assert
+            Assert.Equal("en", result);
+        }
+
+        [Fact]
+        public void GetStringValue_WithDifferentLangValueAttribute_ReturnsLangValue()
+        {
+            // Arrange
+            var value = TestEnum.French;
+
+            // Act
+            var result = value.GetStringValue();
+
+            // Assert
+            Assert.Equal("fr", result);
+        }
+
+        [Fact]
+        public void GetStringValue_WithoutLangValueAttribute_ReturnsEnumName()
+        {
+            // Arrange
+            var value = TestEnum.NoLangValue;
+
+            // Act
+            var result = value.GetStringValue();
+
+            // Assert
+            Assert.Equal("NoLangValue", result);
+        }
+    }
+}
+
+
